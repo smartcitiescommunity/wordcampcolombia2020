2c87564d6fc256d5d811cbeb68571a72b88b9d00
Juan Fernando Villa Hernández <juan.villa@paisdelconocimiento.org>
2020-09-12 02:00:30 -0500

    Update wordpress_install.sh

--- wordpress_install.sh
+++ wordpress_install.sh
@@ -2,10 +2,24 @@
 #Selecciona el sistema operativo Ubuntu 20.04 lts
 #Crea un disco duro de proposito general de 26 Gigas como root "/" y otro de 4 Gigas para el Swap
 sudo mkswap /dev/xvdb
+sudo swapon /dev/xvdb
+#/dev/xvdb swap swap defaults 0 0
+#sudo mkdir /media/swap2/
+#sudo dd if=/dev/zero of=/swap bs=1024 count=1
+#sudo mkswap /swap
+#/swap swap swap defaults 0 0
+sudo sysctl vm.swappiness=10
+#/etc/sysctl.conf
+#en la ultima linea poner
+#vm.swappiness=10
 
 wget -c http://wordpress.org/latest.tar.gz
 tar -xzvf latest.tar.gz
 ls -l
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+php wp-cli.phar --info
+chmod +x wp-cli.phar
+sudo mv wp-cli.phar /usr/local/bin/wp
 sudo cp -R wordpress /var/www/html/wordcamp2020
 ls -l /var/www/html/
 sudo chown -R www-data:www-data /var/www/html/wordcamp2020
